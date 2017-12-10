c12636c65fbf835c627f853a0a860f3b400491a8
Prithivi Raj G <prithiviraj1991@gmail.com>
2017-12-10 22:57:15 +0530

    Added scale manager

--- src/states/boot.state.ts
+++ src/states/boot.state.ts
@@ -5,6 +5,7 @@ import State from './state';
 // The first (boot) state of the game
 export default class BootState extends State {
   create(): void {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.game.state.start('preloader');
   }
 }
